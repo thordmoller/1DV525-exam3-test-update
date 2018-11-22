ccdedadd8d3d96857e4d541baa8cab4521d8436b
Thord Möller <thord_moller_92@hotmail.com>
2018-11-22 22:41:54 +0100

    No empty chat messages

--- src/js/Chat.js
+++ src/js/Chat.js
@@ -8,6 +8,7 @@ export default class Chat extends PwdWindow {
     this.iconUrl = '../image/double-chat-sprite.png'
     this.socket = new window.WebSocket('ws://vhost3.lnu.se:20080/socket/')
     this.displayWindow()
+    this.lastMessageDate = undefined
   }
   /** Extends the initial method with a call to close the socket */
   deleteWindow () {
@@ -31,12 +32,12 @@ export default class Chat extends PwdWindow {
     container.appendChild(clone)
     let textarea = container.querySelectorAll('textarea')[0]
     container.querySelectorAll('input')[0].addEventListener('click', () => {
-      if (textarea !== '') {
+      if (textarea.value !== '') {
         let data = {
           'type': 'message',
           'data': textarea.value,
           'username': window.localStorage.getItem('username'),
-          'channel': 'hej',
+          // 'channel': 'hej',
           'key': 'eDBE76deU7L0H9mEBgxUKVR0VCnq0XBd'
         }
         this.socket.send(JSON.stringify(data))
@@ -54,13 +55,50 @@ export default class Chat extends PwdWindow {
     })
     this.socket.addEventListener('message', event => {
       let data = JSON.parse(event.data)
-      let div = document.createElement('div')
+      let p = document.createElement('p')
       if (data['type'] === 'message' || data['type'] === 'notification') {
-        let output = data['username'] + ' says: ' + data['data']
-        div.appendChild(document.createTextNode(output))
-        let messageDiv = this.element.querySelectorAll('.Messages')[0]
-        messageDiv.appendChild(div)
-        messageDiv.scrollTop = messageDiv.scrollHeight // scroll down the div containing messages
+        if (data['data'] !== '') {
+          let messageDiv = this.element.querySelectorAll('.Messages')[0]
+          let time = new Date()
+          time.setSeconds(0)
+          time.setMilliseconds(0)
+          let output = ''
+          if (this.isNewDate(time)) {
+            output += time.getFullYear() + '-' + time.getMonth() + '-' + time.getDate()
+            p.appendChild(document.createTextNode(output))
+            p.setAttribute('class', 'Username')
+            messageDiv.appendChild(p)
+          }
+          if (time > this.lastMessageDate || this.lastMessageDate === undefined) {
+            let hours = time.getHours()
+            let minutes = time.getMinutes()
+            if (hours < 10) {
+              hours = '0' + hours
+            }
+            if (minutes < 10) {
+              minutes = '0' + minutes
+            }
+            output = hours + ':' + minutes
+            this.lastMessageDate = time
+            p = document.createElement('p')
+            p.appendChild(document.createTextNode(output))
+            p.setAttribute('class', 'Username')
+            messageDiv.appendChild(p)
+          }
+          output = data['username'] + ' says: '
+          p = document.createElement('p')
+          let span = document.createElement('span')
+          span.appendChild(document.createTextNode(output))
+          span.setAttribute('class', 'Username')
+          p.appendChild(span)
+          p.appendChild(document.createElement('br'))
+          p.appendChild(document.createTextNode(data['data']))
+          if (data['username'] === window.localStorage.getItem('username')) {
+            p.setAttribute('class', 'ThisUser')
+          }
+          messageDiv.appendChild(p)
+          messageDiv.scrollTop = messageDiv.scrollHeight // scroll down the div containing messages
+        }
       } else {
         console.log(data)
       }
@@ -82,5 +120,25 @@ export default class Chat extends PwdWindow {
       }
     }.bind(this)
     container.querySelectorAll('input')[1].addEventListener('click', click)
+  }
+  /**
+   * @param {Date} date
+   */
+  isNewDate (date) {
+    let newDate = new Date(date.getTime())
+    newDate.setHours(0)
+    newDate.setMinutes(0)
+    let oldDate
+    if (this.lastMessageDate !== undefined) {
+      oldDate = new Date(this.lastMessageDate.getTime())
+    } else {
+      return true
+    }
+    oldDate.setHours(0)
+    oldDate.setMinutes(0)
+    if (newDate > oldDate) {
+      return true
+    }
+    return false
   }
 }
